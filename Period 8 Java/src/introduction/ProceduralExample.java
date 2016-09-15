aac3d07d6d6146f4272b18b855e20af1bcf04e52
TheJoeJoe <jhur4626@bths.edu>
2016-09-15 13:52:59 -0400

    Added names and years

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -2,6 +2,9 @@ package introduction;
 
 public class ProceduralExample {
     public static void main(String[]args){
+    String[]names = {"Jillian", "Joseph", "Jordan"};
+    String[]years = {"Freshmen", "Sophmore", "Junior", "Senior"};
+    System.out.println("Hi, my name is" + names[0] + "and I am in" + years[0] + ".");  
     System.out.println("Hello, World");  
     System.out.println("Adios");
     }
