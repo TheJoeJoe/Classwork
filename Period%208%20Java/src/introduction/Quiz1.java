c8b46ee75a1dd28b239081ce3c9489c2754f067b
TheJoeJoe <jhur4626@bths.edu>
2016-10-20 13:17:51 -0400

    Add files via upload

---
+++ Period%208%20Java/src/introduction/Quiz1.java
@@ -0,0 +1,68 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class Quiz1 {
+	
+	static Scanner input = new Scanner(System.in);
+	
+		public static String waitForEntry(){
+			return input.nextLine();
+		}
+		
+		//1 point visibility private
+		//1 point for data type declaration(throughout)
+		private static String username = "test_user";
+		private static String password = "test";
+		
+		//1 point for correct method
+		public static void main(String[] args){
+			//1 point, asking username once
+			if(correctUser()){
+				askPassword();
+			}
+			else{
+				System.out.println("Unknown username, " +"please contact the network " + "administrator");
+			}
+		}
+
+		private static void askPassword() {
+			boolean inLoop = true;
+			int triesRemaining = 3;
+			//1 point while loop
+			while(inLoop){
+				System.out.println("Enter your password");
+				//1 point use waitForEntry() correctly
+				String entry = waitForEntry();
+				if(entry.equals(password)){
+					System.out.println("You're in");
+					inLoop = false;
+				}
+				else{
+					//.5 triesRemaining is changing
+					triesRemaining --;
+					if(triesRemaining == 0){
+						//.5 printing invalid password
+						System.out.println("Invalid Password");
+						//.5 point max 3 tries
+						inLoop = false;
+					}
+					else{
+						//.5 point correct number
+						System.out.println("You have" + triesRemaining + "attempt(s) left.");
+					}
+				}
+			}
+		}
+
+		private static boolean correctUser() {
+			System.out.println("Enter a username");
+			//1 point for comparing Strings
+			if(waitForEntry().equals(username)){
+				return true;
+			}
+			return false;
+		}
+		//or do
+		//return waitForEntry().equals(username);
+}
